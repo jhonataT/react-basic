5f9e8b1797fa9299cb65e584edc244a9b3495e53
Jhn <jhonata.tenorio2526@gmail.com>
2021-01-24 18:41:25 -0300

    new index.js and index.html

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const jsxElement = <h1> This is a JSX element (test) </h1>;
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(jsxElement, rootElement);
