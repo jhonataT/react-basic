fd0fd1cdf52c6ca0fe6c324d0141788557b39e64
Jhn <jhonata.tenorio2526@gmail.com>
2021-01-24 20:48:05 -0300

    JSX elements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const jsxElement = <h1> This is a JSX element (test) </h1>;
+// JSX element header:
+const header = (
+  <header>
+    <h1>Welcome to 30 Days Of React</h1>
+    <h2>Getting Started React</h2>
+    <h3>JavaScript Library</h3>
+    <p>Asabeneh Yetayeh</p>
+    <small>Oct 2, 2020</small>
+  </header>
+);
+// JSX element main:
+const main = (
+  <main>
+    <p>Prequesite to get started react.js:</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+  </main>
+);
+// JSX element footer:
+const footer = (
+  <footer>
+    <p>Copyright 2021</p>
+  </footer>
+);
+// JSX element, app, a container or a parent:
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
+);
+
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(jsxElement, rootElement);
+ReactDOM.render(app, rootElement);
