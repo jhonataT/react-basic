6ad4d281b3c8675e841f520b2934e001279e48bd
Jhn <jhonata.tenorio2526@gmail.com>
2021-01-26 12:29:48 -0300

    add image in final test template

--- src/index.js
+++ src/index.js
@@ -1,17 +1,101 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import jhNImage from './images/git-profile.jpeg';
+// To get the root element from the HTML document
+import jhnImage from './images/git-profile.jpeg';
+// JSX element, header
+const welcome = 'Welcome to 30 Days Of React';
+const title = 'Getting Started React';
+const subtitle = 'JavaScript Library';
+const author = {
+  firstName: 'Jhonata',
+  lastName: 'Tenorio',
+};
+const date = 'Jan 26, 2021';
 
-const userStyle = {
-  height: '10px',
-  width: '10px'
-}
+// JSX element, header
+const header = (
+  <header>
+    <div className='header-wrapper'>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>
+        Instructor: {author.firstName} {author.lastName}
+      </p>
+      <small>Date: {date}</small>
+    </div>
+  </header>
+)
+
+const numOne = 3
+const numTwo = 2
+
+const result = (
+  <p>
+    {numOne} + {numTwo} = {numOne + numTwo}
+  </p>
+)
+
+const yearBorn = 1820
+const currentYear = new Date().getFullYear()
+const age = currentYear - yearBorn
+const personAge = (
+  <p>
+    {' '}
+    {author.firstName} {author.lastName} is {age} years old
+  </p>
+)
+
+// JSX element, main
+const techs = ['HTML', 'CSS', 'JavaScript']
+const techsFormatted = techs.map((tech) => <li>{tech}</li>)
 
 const user = (
-  <div> 
-    <img src={jhNImage} alt='Jhn Image'/>
+  <div>
+    <img src={jhnImage} alt='asabeneh image' />
   </div>
-);
+)
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(user, rootElement);
+// JSX element, main
+const main = (
+  <main>
+    <div className='main-wrapper'>
+      <p>
+        Prerequisite to get started{' '}
+        <strong>
+          <em>react.js</em>
+        </strong>
+        :
+      </p>
+      <ul>{techsFormatted}</ul>
+      {result}
+      {personAge}
+      {user}
+    </div>
+  </main>
+)
+
+const copyRight = 'Copyright 2020'
+
+// JSX element, footer
+const footer = (
+  <footer>
+    <div className='footer-wrapper'>
+      <p>{copyRight}</p>
+    </div>
+  </footer>
+)
+
+// JSX element, app
+const app = (
+  <div className='app'>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+// we render the JSX element using the ReactDOM package
+ReactDOM.render(app, rootElement)
